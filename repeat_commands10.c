ea5ba35053f6ff38e52584b4757ec8c58bc24932
B1ackMeteorite <117219991+B1ackMeteorite@users.noreply.github.com>
2023-02-05 21:24:24 -0300

    Create repeat_commands10.c

---
+++ repeat_commands10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+int x,y,i;
+y=0;
+x=0;
+for(i=0;i<=50;i++){
+x=y+x;
+y=y+2;
+
+
+
+
+
+
+}
+printf("This is the sum of the first 50 even numbers: %d ",x);
+
+return 0;
+}
