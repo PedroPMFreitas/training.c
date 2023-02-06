291e0c5a4f5045ee06de5876f6d2bf742b8a07dc
B1ackMeteorite <117219991+B1ackMeteorite@users.noreply.github.com>
2023-02-05 21:00:40 -0300

    Create repeat_commands11.c

---
+++ repeat_commands11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+int x,y,i;
+y=-1;
+printf("escreva a quantidade de numeros impares que vc quer:");
+scanf("%d",&x);
+for(i=x;i>0;i--){
+y=y+2;
+printf("%d ",y);
+
+
+
+
+
+}
+
+return 0;
+}
